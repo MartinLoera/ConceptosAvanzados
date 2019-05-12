2de19c498719d671557c86b6c1881d186b67802b
MartinLoera <46764356+MartinLoera@users.noreply.github.com>
2019-05-11 19:01:01 -0600

    Add files via upload

---
+++ BDatos.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include <string.h>
+using namespace std;
+
+int main(){
+
+system("color a");
+ifstream archivo;
+
+try{
+    archivo.open("lc5.txt");
+    if(archivo.fail()) throw "No esta dispnible la base de datos.";
+}catch(const char * mensaje){
+    cout<<">>Error: "<<mensaje;
+    system("pause>nul");
+    exit(0);
+}catch(...){}
+
+string matricula;
+string nombre;
+int n;
+
+string dato;
+
+system("cls");
+
+cout<<"Ingrese su nombre o matricula: ";
+getline(cin,dato);
+
+for(int i = 0; i<dato.size(); i++){
+    dato[i] = toupper(dato[i]);
+}
+
+while(!archivo.eof()){
+
+    getline(archivo,matricula);
+    nombre = matricula;
+
+    string aux;
+
+    for(int i=0; i<6; i++){
+        aux+=matricula[i];
+    }
+    matricula=aux;
+    aux.clear();
+
+    for(int i=7; i<nombre.size(); i++){
+        aux+=nombre[i];
+    }
+    nombre=aux;
+    //cout<<nombre;system("pause");
+
+    if(dato==matricula) break;
+    if(dato==nombre) break;
+}
+
+system("cls");
+
+if(dato==nombre || dato==matricula){
+    cout<<"Existe"<<endl;
+}else{
+    cout<<"No Existe"<<endl;
+    system("pause");
+    exit(0);
+}
+
+cout<<"Matricula: "<<matricula<<endl<<"Nombre: "<<nombre<<endl;
+archivo.close();
+return 0;
+}
